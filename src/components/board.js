3ed350c9e8041290b4144b7099b06a2d11864ef3
Ervin Gonzales <ervinkleitz@gmail.com>
2019-07-01 22:17:01 -0700

    Some improvements and added diagonal winning check

--- src/components/board.js
+++ src/components/board.js
@@ -9,39 +9,46 @@ class Board extends React.Component {
       rows: 6,
       columns: 7,
       grids: [],
-      currentPlayer: 1 // player 1 is black
+      currentPlayer: 1, // player 1 is black
+      hasWinner: false
     };
+  }
 
+  componentDidMount() {
     this.constructBoard();
   }
    
   render() {
 
-    const Row = (props) => {
-      return props.row.map((column, columnIndex) => {
-
-        const id = props.rowindex.toString() + columnIndex;
-        const grid = 
-          <div className='grid' 
-            key={columnIndex}
-            id={id}
-            onClick={event => this.setGrid(event, {row: props.rowindex, column:columnIndex})}>
-          </div>;
-
-        return grid;
-      });
-    }
-
     return (
       <div>
         <h3>BOARD</h3>
-        {this.state.grids.map((row, rowIndex) => {
-          return (
-            <div key={rowIndex}>
-              <Row row={row} rowindex={rowIndex}/>
-            </div>
-          )
-        })}
+        {this.state.hasWinner &&
+          <h2>
+            {this.state.currentPlayer === 1 ? 'BLACK' : 'RED'} player has WON!
+          </h2>
+        }
+
+        <button onClick={() => this.restartGame()}>Restart Game</button>
+
+        <div className='board'>
+          {this.state.grids.map((row, rowIndex) => {
+            return (
+              <div key={rowIndex}>
+                {row.map((column, columnIndex) => {
+                  const id = rowIndex.toString() + columnIndex;
+                  return  <div className='grid' 
+                    key={id}
+                    id={id}
+                    onClick={() => 
+                      this.setGrid({row: rowIndex, column:columnIndex})
+                    }>
+                  </div>;
+                })}
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
@@ -49,14 +56,17 @@ class Board extends React.Component {
   /**
    * @desc Set the chip on the board
    */
-  setGrid = (event, coordinates) => {
+  setGrid = (coordinates) => {
+    // Prevent any more actions when a winner has been found
+    if(this.state.hasWinner){
+      return;
+    }
     // find bottom of column
     let bottomEmptyRow;
     let gridValue;
     let grid;
-    let hasWinner;
 
-    for( let row = this.state.rows -1; row !== 0; row--) {
+    for( let row = this.state.rows -1; row > -1; row--) {
       let coordinateValue = this.state.grids[row][coordinates.column];
       if(coordinateValue === 0) {
         bottomEmptyRow = row;
@@ -66,7 +76,7 @@ class Board extends React.Component {
     }
 
     // If no empty grid is found, return.
-    if(!bottomEmptyRow){
+    if(bottomEmptyRow === undefined) {
       return;
     }
 
@@ -77,21 +87,23 @@ class Board extends React.Component {
     if( gridValue === 0){
       const chipColor = this.state.currentPlayer === 1 ? 'red' : 'black';
       grid = document.getElementById(bottomEmptyRow.toString() + coordinates.column);
-      this.state.grids[bottomEmptyRow][coordinates.column] = this.state.currentPlayer;
-      
       grid.classList.add(chipColor);
 
-      // check for winner
-      hasWinner = this.checkForWinnder({row: bottomEmptyRow, column: coordinates.column});
+      let _grids = this.state.grids;
+      _grids[bottomEmptyRow][coordinates.column] = this.state.currentPlayer;
+      
+      this.setState({
+        grids: _grids
+      });
 
-      if(hasWinner) {
-        const player = this.state.currentPlayer === 1 ? 'Red' : 'Black';
-        const message = player + ' has won!';
-        alert(message);
-        window.location.reload();
-      } else {
-        this.state.currentPlayer = this.state.currentPlayer === 1 ? -1 : 1;
-      }
+      // check for winner
+      this.setState({
+        hasWinner: this.checkForWinner({row: bottomEmptyRow, column: coordinates.column})
+      }, () => {
+        this.setState({
+          currentPlayer: this.state.currentPlayer === 1 ? -1 : 1
+        });
+      });
     } 
   }
 
@@ -99,14 +111,24 @@ class Board extends React.Component {
    * @desc Construct Board in HTML
    */
   constructBoard = () => {
+    let grids = [];
     let grid = [];
+
     for(let row = 0; row < this.state.rows; row++) {
       for(let column = 0; column < this.state.columns; column++) {
         grid.push(0);
       }
-      this.state.grids.push(grid);
+      grids.push(grid);
       grid = [];
     }
+
+    this.setState({
+      grids: grids
+    });
+  }
+
+  restartGame() {
+    window.location.reload();
   }
 
   /**
@@ -114,9 +136,10 @@ class Board extends React.Component {
    * @param {Object} coordinates 
    * @returns {Boolean}
    */
-  checkForWinnder(coordinates) {
+  checkForWinner(coordinates) {
     return this.checkForVerticalMatches(coordinates) ||
-      this.checkHorizontalMatches(coordinates);
+      this.checkHorizontalMatches(coordinates) || 
+      this.checkForDiagonalMatches(coordinates);
   }
 
   /**
@@ -128,7 +151,7 @@ class Board extends React.Component {
     let winner = false;
     let sets = [];
   
-    for(let col = 0; col <= 2; col++) {
+    for(let col = 0; col <= 3; col++) {
       let start = col;
       let set = [];
       
@@ -174,6 +197,91 @@ class Board extends React.Component {
       if(sum === 4 || sum === -4) {
         winner = true;
         break;
+      }
+    }
+    return winner;
+  }
+
+  /**
+   * @desc Check for diagonal winning combinations
+   * @param {Object} coordinates 
+   */
+  checkForDiagonalMatches(coordinates) {
+    let winner = false;
+    // list of sets, one going top to bottom from the left and
+    // one going bottom to top also from the left
+    let sets = []; 
+    let set = [];
+    let startRow;
+    let startColumn;
+
+    // find start of diagonal
+    startRow = coordinates.row;
+    startColumn = coordinates.column;
+    // going upperleft
+    while(
+      this.state.grids[startRow] &&
+      this.state.grids[startColumn]
+    ){
+      set.unshift(this.state.grids[startRow][startColumn]);
+      startColumn--;
+      startRow--;
+    }
+
+    // going lower left
+    startRow = coordinates.row + 1;
+    startColumn = coordinates.column + 1;
+    while(
+      this.state.grids[startRow] &&
+      this.state.grids[startRow][startColumn]
+    ){
+      set.push(this.state.grids[startRow][startColumn]);
+      startColumn++;
+      startRow++;
+    }
+    // add set to list of sets
+    sets.push(set);
+
+    // going upper right
+    startRow = coordinates.row;
+    startColumn = coordinates.column;
+    set = [];
+    while(
+      this.state.grids[startRow] &&
+      this.state.grids[startColumn]
+    ){
+      set.push(this.state.grids[startRow][startColumn]);
+      startColumn++;
+      startRow--;
+    }
+
+    // going lower left
+    startRow = coordinates.row;
+    startColumn = coordinates.column;
+    while(
+      this.state.grids[startRow] &&
+      this.state.grids[startRow][startColumn]
+    ){
+      set.unshift(this.state.grids[startRow][startColumn]);
+      startColumn--;
+      startRow++;
+    }
+    // add the diagonal (lower left to upper right) to the list of
+    // sets to check for winning combos
+    sets.push(set);
+
+    // check all the combinations if any of them are winning combos
+    for(let setIndex = 0; setIndex < sets.length; setIndex++) {
+      let _set = sets[setIndex].slice();
+      let position = 0;
+      while(position + 3 < _set.length) {
+        let setToCheck = _set.slice().splice(position, 4);
+        let sum = setToCheck.reduce((a, b) => a + b, 0);
+        if(sum === 4 || sum === -4) {
+          winner = true;
+          break;
+        }
+        position++;
       }
     }
     return winner;
